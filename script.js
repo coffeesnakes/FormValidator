2873217a3ec03a83b36a202b22928e018fbeb709
Jacob Berg <coffeesnakes@gmail.com>
2020-10-23 17:05:09 -0700

    showError function properly displaying when invalid input

--- script.js
+++ script.js
@@ -5,17 +5,22 @@ const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
-
+// show inputted error msg
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error'
+}
+// event Listeners
 form.addEventListener('submit', function (event) {
   event.preventDefault();
 
-if (username.value === '') {
-  showError(username, 'Username is required')
-} else {
-  showSuccess(username);
-}
+  if (username.value === '') {
+    showError(username, 'Username is required')
+  } else {
+    showSuccess(username);
+  }
 })
 
-function Authenticate () {
+function Authenticate() {
 
 }

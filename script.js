bb6a2c99cbc21d021b711feb4ad2be24c5e999fd
Jacob Berg <coffeesnakes@gmail.com>
2020-10-23 17:01:57 -0700

    variables set up to access DOM elements and submit event listener

---
+++ script.js
@@ -0,0 +1,21 @@
+
+// this accesses the document object model (DOM) and binds it to the form variable in js
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+if (username.value === '') {
+  showError(username, 'Username is required')
+} else {
+  showSuccess(username);
+}
+})
+
+function Authenticate () {
+
+}
